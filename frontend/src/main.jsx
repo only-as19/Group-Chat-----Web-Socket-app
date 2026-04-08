c02bdebe5a809ad10d4a19671aafa48b96f49e63
Asad <hellohello51981@gmail.com>
2026-04-08 16:27:30 +0500

    ready for deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './app.css';
+import './App.css';
 import App from './App.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
